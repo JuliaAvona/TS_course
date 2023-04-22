57aea90fe9a4bfc36ac6c9f457c87ecf71e6eebe
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-04-21 21:49:43 -0600

    init: lesson 7 TS

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-const str = 'str';
+let userName: string = 'Julia';
+let password: number = 123131231;
+let email: boolean = false;
 
-console.log(str);
+console.log(userName);
+
+userName = 'Alex';
+
+// string - "", '', ``;
+// number - 10, 0.5, 0.00000002, 10e34, -45
+// boolean - true, false
+
+const isBirthday: boolean = true;
+const age: number = 40;
+const newName: string = 'John';
+
+if (isBirthday) {
+  console.log(`Happy birthday ${newName.toLocaleUpperCase()}. Today you are ${age + 1} years old!`);
+}
